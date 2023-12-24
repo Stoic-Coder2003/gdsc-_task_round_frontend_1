d22ebac60c92a776a47084914393acdc0777b700
Stoic-Coder2003 <shubhmehrotra.personal@gmail.com>
2023-12-24 15:55:06 +0530

    changes made to the cursor animations

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
